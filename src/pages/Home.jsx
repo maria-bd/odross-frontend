6ffb424f77e5048f24825ef1db44f99750ba6f9f
Maria Boudeheb <135654272+maria-bd@users.noreply.github.com>
2024-04-18 03:38:21 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
+import About from '../components/About';
+import UserList from '../components/UserList';
 import Footer from '../components/Footer';
+
+
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <UserList />
       <HeroSection />
+      <About />
       <Footer />
     </div>
   );
