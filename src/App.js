57a92907a508f5e97fb3332234de87a8d4c849d5
Maria Boudeheb <135654272+maria-bd@users.noreply.github.com>
2024-05-01 19:55:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
-import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import Login from './components/Login';
+import { BrowserRouter as Router, Routes, Route,  } from 'react-router-dom'; // Import Routes
+//import Navbar from './components/Navbar';
+//import HeroSection from './components/HeroSection';
+import Register from './components/Register';
+import Login1 from './components/Login1';
+import Login2 from './components/Login2';
+import Home from './pages/Home';
+import GradientBackground from './components/GradientBackground';
+
 
 function App() {
   return (
     <Router>
       <div>
-        <Navbar />
         <Routes> {/* Wrap Routes around your Route components */}
-          <Route path="/" element={<HeroSection />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+                <Route path="/login1" element={<GradientBackground><Login1 /></GradientBackground>} />
+                <Route path="/login2" element={<GradientBackground><Login2 /></GradientBackground>} />
+                <Route path="/register" element={<GradientBackground><Register /></GradientBackground>} />
           {/* Add other routes as needed */}
         </Routes>
       </div>
