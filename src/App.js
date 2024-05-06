b0a03a84a89bb48319d2f6ea18a020d88d661470
Maria Boudeheb <135654272+maria-bd@users.noreply.github.com>
2024-05-06 15:17:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route,  } from 'react-router-dom'; // Import Routes
-//import Navbar from './components/Navbar';
-//import HeroSection from './components/HeroSection';
+import Home from './pages/Home';
 import Register from './components/Register';
 import Login1 from './components/Login1';
 import Login2 from './components/Login2';
-import Home from './pages/Home';
+import Login3 from './components/Login3';
 import GradientBackground from './components/GradientBackground';
+import Profile from './components/Profile'; // Assuming Profile component is defined
+import EditProfile from './components/EditProfile'; // Assuming Profile component is defined
 
 
 function App() {
@@ -16,9 +17,14 @@ function App() {
       <div>
         <Routes> {/* Wrap Routes around your Route components */}
           <Route path="/" element={<Home />} />
-                <Route path="/login1" element={<GradientBackground><Login1 /></GradientBackground>} />
-                <Route path="/login2" element={<GradientBackground><Login2 /></GradientBackground>} />
-                <Route path="/register" element={<GradientBackground><Register /></GradientBackground>} />
+          <Route path="/Login1" element={<GradientBackground><Login1 /></GradientBackground>} />
+          <Route path="/Login2" element={<GradientBackground><Login2 /></GradientBackground>} />
+          <Route path="/Register" element={<GradientBackground><Register /></GradientBackground>} />
+          <Route path="/Profile" element={<GradientBackground><Profile /></GradientBackground>} />
+          <Route path="/EditProfile" element={<GradientBackground><EditProfile /></GradientBackground>} />
+          <Route path="/admin" element={<GradientBackground><Login3 /></GradientBackground>} />
+
+
           {/* Add other routes as needed */}
         </Routes>
       </div>
